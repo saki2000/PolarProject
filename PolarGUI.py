9f0b739eadb2731e60d6da9d4348f0ef2884e3a1
saki2000 <saki2000@wp.pl>
2021-11-30 21:30:30 +0000

    tidy up

--- PolarGUI.py
+++ PolarGUI.py
@@ -1,23 +1,15 @@
 import tkinter as tk
+import threading
 from tkinter import filedialog
-from PolarProjectMotors import *
-
-
-
-
-
-#function opening files to read
-def openFile():
-    dataFile = filedialog.askopenfilename(filetypes=(("HPGL Files", "*.hpgl"),))
-    dataFile = open(dataFile, 'r')
-    DATAPOINTS = dataFile.read()
-    dataFile.close()
-
-
-
+from tkinter.constants import DISABLED
+from types import LambdaType
+from PolarProjectMotors import StepperMotor
 
 class App:
     def __init__(self, root):
+        self.leftStepper = StepperMotor(15, 18, 23, 24)
+        self.rightStepper = StepperMotor(25, 8, 12, 16)
+
         #setting title
         root.title("Polar Project")
         #setting window size
@@ -30,13 +22,64 @@ class App:
         root.resizable(width=False, height=False)
 
         #open hpgl file button
-        btnOpenFile=tk.Button(root, text="Open file", command=openFile) 
+        btnOpenFile=tk.Button(root, text="Open file", command=self.openFile) 
         btnOpenFile.place(x=700,y=470,width=70,height=25)
 
         #execute loaded project button
-        btnExecute=tk.Button(root, text="Execute", command=sample) 
-        btnExecute.place(x=300,y=270,width=60,height=60)
+        self.btnExecute=tk.Button(root, text="Execute", command=self.sample) 
+        self.btnExecute.place(x=300,y=270,width=60,height=60)
 
+        #function opening files to read
+    def openFile(self):
+        self.dataFile = filedialog.askopenfilename(filetypes=(("HPGL Files", "*.hpgl"),))
+        self.dataFile = open(self.dataFile, 'r')
+        DATAPOINTS = self.dataFile.read()
+        self.dataFile.close()
+
+        #function disable buttons
+    def disableButtons(self):
+        self.btnExecute['state'] = 'disabled'
+        self.dataFile['state'] = 'disabled'
+
+        #function enable buttons
+    def enableButtons(self):
+        self.btnExecute['state'] = 'normal'
+        self.btnExecute['state'] = 'normal'
+
+
+    #function calling in execution of stepper 
+    #motors in parralel
+    def stepperMotorsCall(self,lDirection,lNoofSteps,lSpeed,rdirection,rNoOfSteps,rSpeed):
+
+        #creating threads for each stepper
+        leftStepperThread = threading.Thread(target  = self.leftStepper.stepperControl, args=[lDirection,lNoofSteps,lSpeed], daemon = True )
+        rightStepperThread = threading.Thread(target = self.rightStepper.stepperControl, args=[rdirection,rNoOfSteps,rSpeed], daemon = True)
+
+        leftStepperThread.start()
+        rightStepperThread.start()
+
+        #joining threads
+        leftStepperThread.join()
+        rightStepperThread.join()
+    
+    #lambda function spawning a new dameon thread that will spawn 
+    #threads and execute stepper motor calls
+    def sample(self):
+        executeSampleThreadLambda = lambda:(
+            self.disableButtons(),
+            self.stepperMotorsCall("left", 150,0.001, "left", 400,0),
+            self.stepperMotorsCall("left", 50,0, "right", 200,0.0005),
+            self.stepperMotorsCall("right", 500,0, "right", 500,0.005),
+            self.stepperMotorsCall("left", 500,0.005, "left", 500,0),
+            self.stepperMotorsCall("right", 500,0, "right", 100,0.0005),
+            self.stepperMotorsCall("right", 400,0.0007, "right", 200,0),
+            self.stepperMotorsCall("left", 500,0.0001, "left", 500,0),
+            self.stepperMotorsCall("right", 400,0, "left", 200,0.0001),
+            self.stepperMotorsCall("left", 200,0, "right", 600,0.0005),
+            self.enableButtons())
+
+        executeSampleThread = threading.Thread(target = executeSampleThreadLambda, daemon=True)
+        executeSampleThread.start()
     
 
 #main menu
--- PolarProjectMotors.py
+++ PolarProjectMotors.py
@@ -1,8 +1,6 @@
 import RPi.GPIO as GPIO
 import time
-import threading
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
+
 
 
 class StepperMotor():               #Stepper Motor Class - Creating stepper motor object
@@ -11,7 +9,9 @@ class StepperMotor():               #Stepper Motor Class - Creating stepper moto
     HALFSTEP_SEQ = [[0,1,0,0], [0,1,0,1], [0,0,0,1], [1,0,0,1], [1,0,0,0], [0,0,1,1], [0,0,1,0], [0,1,1,0]]
     
     #Constructor taking pin numbers that stepper is connected to raspberry pi
-    def __init__(self, pin_a, pin_b, pin_c, pin_d):     
+    def __init__(self, pin_a, pin_b, pin_c, pin_d):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)     
         self.step_index = 1
         self.control_pins = [pin_a, pin_b, pin_c, pin_d]
         for pin in range(len(self.control_pins)):
@@ -48,78 +48,25 @@ class StepperMotor():               #Stepper Motor Class - Creating stepper moto
         self.halfStepRight(speed)
         self.halfStepRight(speed)
 
-#function controling left stepper motor
-#executes number of steps in given direction and speed
-def leftStepperControl(direction, noOfSteps, speed):
+    #function controling left stepper motor
+    #executes number of steps in given direction and speed
+    def stepperControl(self,direction, noOfSteps, speed):
 
-    pointComplete = False
+        pointComplete = False
 
-    if noOfSteps == 0:
-        pointComplete = True
-
-    while pointComplete == False:   #while loop to execute all steps passed 
-
-        if(direction == "left"):    #if statment checking for direction
-            leftStepper.stepLeft(speed)
-            noOfSteps -= 1           
-        else:
-            leftStepper.stepRight(speed)
-            noOfSteps -= 1   
-        if noOfSteps == 0:
-            pointComplete = True    #changing bool and ending while loop
-
-#function controling right stepper motor
-#executes number of steps in given direction and speed
-def rightStepperControl(direction, noOfSteps, speed):
-
-    pointComplete = False
-
-    if noOfSteps == 0:
-        pointComplete = True
-
-    while pointComplete == False:
-
-        if(direction == "left"):
-            rightStepper.stepLeft(speed)
-            noOfSteps -= 1
-        else:
-            rightStepper.stepRight(speed)
-            noOfSteps -= 1   
         if noOfSteps == 0:
             pointComplete = True
 
-#function calling in execution of stepper 
-#motors in parralel
-def stepperMotorsCall(lDirection,lNoofSteps,lSpeed,rdirection,rNoOfSteps,rSpeed):
-    #creating threads for each stepper
-    leftStepperThread = threading.Thread(target  = leftStepperControl, args=[lDirection,lNoofSteps,lSpeed] )
-    rightStepperThread = threading.Thread(target = rightStepperControl, args=[rdirection,rNoOfSteps,rSpeed])
+        while pointComplete == False:   #while loop to execute all steps passed 
 
-    leftStepperThread.start()
-    rightStepperThread.start()
-
-    #joining threads
-    leftStepperThread.join()
-    rightStepperThread.join()
-
-
-def sample():
-    stepperMotorsCall("left", 150,0.001, "left", 400,0)
-    stepperMotorsCall("left", 50,0, "right", 200,0.0005)
-    stepperMotorsCall("right", 500,0, "right", 500,0.005)
-    stepperMotorsCall("left", 500,0.005, "left", 500,0)
-    stepperMotorsCall("right", 500,0, "right", 100,0.0005)
-    stepperMotorsCall("right", 400,0.0007, "right", 200,0)
-    stepperMotorsCall("left", 500,0.0001, "left", 500,0)
-    stepperMotorsCall("right", 400,0, "left", 200,0.0001)
-    stepperMotorsCall("left", 200,0, "right", 600,0.0005)
-
-
-#main menu
-if __name__ == "__main__":
-
-    leftStepper = StepperMotor(15, 18, 23, 24)
-    rightStepper = StepperMotor(25, 8, 12, 16)
+            if(direction == "left"):    #if statment checking for direction
+                self.stepLeft(speed)
+                noOfSteps -= 1           
+            else:
+                self.stepRight(speed)
+                noOfSteps -= 1   
+            if noOfSteps == 0:
+                pointComplete = True    #changing bool and ending while loop
 
     
 
