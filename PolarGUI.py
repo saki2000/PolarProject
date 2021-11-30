160d2f7825c0325eade5923757504fa1c6a3f399
saki2000 <saki2000@wp.pl>
2021-11-30 18:43:05 +0000

    new v

--- PolarGUI.py
+++ PolarGUI.py
@@ -1,9 +1,14 @@
 import tkinter as tk
 from tkinter import filedialog
+from PolarProjectMotors import *
+
+
+
+
 
 #function opening files to read
 def openFile():
-    dataFile = filedialog.askopenfilename(filetypes=(("HPGI Files", "*.hpgi"),))
+    dataFile = filedialog.askopenfilename(filetypes=(("HPGL Files", "*.hpgl"),))
     dataFile = open(dataFile, 'r')
     DATAPOINTS = dataFile.read()
     dataFile.close()
@@ -24,15 +29,19 @@ class App:
         root.geometry(alignstr)
         root.resizable(width=False, height=False)
 
+        #open hpgl file button
         btnOpenFile=tk.Button(root, text="Open file", command=openFile) 
         btnOpenFile.place(x=700,y=470,width=70,height=25)
 
- 
+        #execute loaded project button
+        btnExecute=tk.Button(root, text="Execute", command=sample) 
+        btnExecute.place(x=300,y=270,width=60,height=60)
 
-    def btnOpenFile_command(self):
-        print("command")
+    
 
+#main menu
 if __name__ == "__main__":
+
     root = tk.Tk()
     app = App(root)
     root.mainloop()
