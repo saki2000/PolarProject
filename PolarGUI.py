7ce2a6b74713a18b10c92374d4691645b151f5ee
saki2000 <saki2000@wp.pl>
2022-04-27 11:19:46 +0100

    gui improved

--- PolarGUI.py
+++ PolarGUI.py
@@ -15,8 +15,8 @@ class App:
         #setting title
         root.title("Polar Project")
         #setting window size
-        width=841
-        height=562
+        width=800
+        height=600
         screenwidth = root.winfo_screenwidth()
         screenheight = root.winfo_screenheight()
         alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
@@ -24,36 +24,63 @@ class App:
         root.resizable(width=False, height=False)
 
 
-        #open hpgl file button
-        self.btnOpenFile=tk.Button(root, text="Open file", command=self.openFile) 
-        self.btnOpenFile.place(x=700,y=470,width=70,height=25)
+        #exit button
+        self.btnExit=tk.Button(root, text="EXIT") 
+        self.btnExit.place(x=700,y=500,width=80,height=80)
+      
+
+        #Move Gondola Up button
+        self.btnGondolaUp=tk.Button(root, text="Up") 
+        self.btnGondolaUp.place(x=110,y=20,width=60,height=80)
+
+
+        #Move Gondola down button
+        self.btnGondolaDown=tk.Button(root, text="Down") 
+        self.btnGondolaDown.place(x=110,y=140,width=60,height=80)
+
+
+        #Move Gondola left button
+        self.btnGondolaLeft=tk.Button(root, text="Left") 
+        self.btnGondolaLeft.place(x=20,y=80,width=80,height=60)
+
+
+        #Move Gondola Right button
+        self.btnGondolaRight=tk.Button(root, text="Right") 
+        self.btnGondolaRight.place(x=180,y=80,width=80,height=60)
+
+
+        #pen Up button
+        self.btnServoDown=tk.Button(root, text="Pen Up", command=self.servoUp) 
+        self.btnServoDown.place(x=300,y=40,width=80,height=80)
+
+
+        #pen Down button
+        self.btnServoUp=tk.Button(root, text="Pen Down", command=self.servoDown) 
+        self.btnServoUp.place(x=300,y=160,width=80,height=80)
 
 
         #execute loaded project button
         self.btnExecute=tk.Button(root, text="Execute", command=self.executeDrawing) 
-        self.btnExecute.place(x=300,y=270,width=60,height=60)
-
-
-        #servo test button down
-        self.btnServoDown=tk.Button(root, text="servoDown", command=self.servoDown) 
-        self.btnServoDown.place(x=400,y=300,width=60,height=60)
-
-        #servo test button down
-        self.btnServoUp=tk.Button(root, text="servoUp", command=self.servoUp) 
-        self.btnServoUp.place(x=470,y=350,width=60,height=60)
+        self.btnExecute.place(x=40,y=420,width=100,height=100)
 
 
         #button stoping motors
         self.btnStopMotors=tk.Button(root, text="Stop", command=self.stopMotors) 
-        self.btnStopMotors.place(x=200,y=150,width=60,height=60)
+        self.btnStopMotors.place(x=180,y=440,width=80,height=80)
 
 
-    #testing servo down
+        #open hpgl file button
+        self.btnOpenFile=tk.Button(root, text="Open file", command=self.openFile) 
+        self.btnOpenFile.place(x=650,y=40,width=120,height=40)
+
+
+
+    #setting servo position down
     def servoDown(self):
         self.gondola.penDown()
-    #testing servo up
 
 
+    #setting servo position up
     def servoUp(self):
         self.gondola.penUp()
 
@@ -65,7 +92,7 @@ class App:
 
     # buttong stopping execution
     def stopMotors(self):
-        self.rightStepper.stopMotors()
+        self.proccessData.rightStepper.stopMotors()
         self.enableButtons()
 
 
